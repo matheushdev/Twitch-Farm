b264a4e0ba43e34cda7686db67cdf08e3ef2f7ae
Matheus Henrique <matheushenrique_cunha@hotmail.com>
2020-05-25 22:11:37 -0300

    Remove on/off button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let h = `${d.getHours()}`.padStart(2, '0')
 let m = `${d.getMinutes()}`.padStart(2, '0')
 let s = `${d.getSeconds()}`.padStart(2, '0')
 var time = `${h}:${m}:${s}`;
+var i = 1;
 
 async function farmBox() {
     setInterval(() => {
@@ -10,36 +11,12 @@ async function farmBox() {
         if (!box) {
             console.log(`[${time}] Não encontrou nada...\n`);
         } else {
+            var qnt = i++; 
             console.log(`[${time}] Encontrou recompensa.\n`);
             box.click();
-            console.log(`[${time}] Recompensa pega.\n`);
+            console.log(`[${time}] Recompensa pega. (TOTAL: ${qnt})\n`);
         }
-    }, 60000);
+    }, 5000);
 }
 
-const interval = setInterval(() => {
-    const header = document.querySelector('.community-points-summary');
-    if (header) {
-        clearInterval(interval);
-
-        const button = document.createElement("button");
-        button.innerHTML = 'ON';
-        button.classList.add('btn-on');
-        button.addEventListener('click', () => {
-            console.log(`[${time}] Auto farm ON.\n`);
-            farmBox();
-        });
-        header.appendChild(button);
-
-
-        /*
-            const button2x = document.createElement("button");
-            button2x.innerHTML = 'OFF';
-            button2x.classList.add('btn-off')
-            button2x.addEventListener('click', () => {
-                console.log(`[${time}] Auto farm OFF.\n`);
-            });
-            header.appendChild(button2x);
-        */
-    }
-}, 1000)
+farmBox();
